8b6a7f21dfe399a40aa2a78b8fc405227c32693a
Vinicius dos Santos <143943952+viniciusdossantosss@users.noreply.github.com>
2026-04-07 20:05:18 -0300

    Enhance Musica class with tonalidades and Key
    Added tonalidades array and Key property to Musica class.

--- Curso-Alura/Screensound-AprendendoApi/Screensound-AprendendoApi/Modelos/Musica.cs
+++ Curso-Alura/Screensound-AprendendoApi/Screensound-AprendendoApi/Modelos/Musica.cs
@@ -1,9 +1,11 @@
-﻿using System.Text.Json.Serialization;
+using System.Text.Json.Serialization;
 
 namespace Screensound_AprendendoApi.Modelos;
 
 internal class Musica
 {
+
+    private string[] tonalidades = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
     [JsonPropertyName("song")]
     public string? Nome { get; set; }
     [JsonPropertyName("artist")] 
@@ -12,6 +14,13 @@ internal class Musica
     public int Duracao { get; set; }
     [JsonPropertyName("genre")]
     public string? Genero { get; set; }
+    [JsonPropertyName("key")]
+    public int Key { get; set; }
+    public string Tonalidade { 
+        get{
+            return tonalidades[Key];
+        } 
+    }
 
     public void ExibirDetalhesDaMusica()
     {
@@ -19,5 +28,6 @@ internal class Musica
         Console.WriteLine($"Artista: {Artista}");
         Console.WriteLine($"Duração em segundo: {Duracao/1000}");
         Console.WriteLine($"Gênero: {Genero}");
+        Console.WriteLine($"Tonalidade: {Tonalidade}");
     }
 }
